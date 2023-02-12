3368eae03662ee3bd97fda1e95e657191f81a0b8
javeria <fa22-bcs-001>
2023-02-12 12:02:27 +0500

    Web dev course

---
+++ JS/7_2_Function.js
@@ -0,0 +1,31 @@
+console.log("This is js file");
+let lang1 = "java";
+let lang2 = "Html";
+let lang3 = "Python";
+let lang4 = "JavaScript";
+let lang5 = "Css"
+console.log(lang1+" is a nice language");
+console.log(lang2+" is a nice language");
+console.log(lang3+" is a nice language");
+console.log(lang4+" is a nice language");
+console.log(lang5+" is a nice language");
+
+// We can short this work by finctions:
+
+function programming(lang,greet) {
+    console.log(greet+ " " + lang)
+   console.log(lang+" Is a nice language");
+}
+{
+let lang1 = "java";
+let lang2 = "Html";
+let lang3 = "Python";
+let lang4 = "JavaScript";
+let lang5 = "Css";
+let greet = "Good Morning";
+programming(lang1,greet);
+programming(lang2,greet);
+programming(lang3,greet);
+programming(lang4,greet);
+programming(lang5,greet);
+}
