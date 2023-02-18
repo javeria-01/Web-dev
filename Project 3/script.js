b50e43e8b3ed721ff9582b4cf68148ea752687c1
javeria <fa22-bcs-001>
2023-02-18 18:26:07 +0500

    Web dev course

---
+++ Project 3/script.js
@@ -0,0 +1,16 @@
+function Alert() {
+    alert("You must br 18+ For visiting this website." );
+}
+setTimeout(Alert,4000);
+
+
+function age() {
+    let Age = prompt("What is your age")
+    if(Age >=18){
+        alert("Ok so you are 18+, Welcome to my website")
+    }
+    else if(Age <18){
+        alert("Sorry! You are too young for this website...")
+    }
+}
+setTimeout(age,6000);
